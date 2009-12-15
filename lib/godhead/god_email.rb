21fec1c3c74b3c746cc0b75b46f95fd08c5acc76
Philip (flip) Kromer <flip@infochimps.org>
2009-12-14 20:41:32 -0600

    email config

--- lib/godhead/god_email.rb
+++ lib/godhead/god_email.rb
@@ -1,10 +1,11 @@
 module God
-  def self.setup_email options, &block
-    God::Contacts::Email.message_settings = {
-      :from           => options[:from], }
+  #
+  # Sets up
+  #
+  def self.add_simple_contact to_address, group=nil, &block
     God.contact(:email) do |c|
-      c.email         = options[:to]
-      c.group         = options[:group] || 'default'
+      c.email         = to_address
+      c.group         = group || 'default'
       block.call(c) if block
     end
   end
@@ -12,10 +13,11 @@ module God
   #
   # GMail
   #
+  # Sends mail using a gmail or google apps account.
+  #
   # http://millarian.com/programming/ruby-on-rails/monitoring-thin-using-god-with-google-apps-notifications/
-  def self.notify_by_gmail options, &block
+  def self.notify_by_gmail options
     require 'tlsmail'
-    setup_email options, &block
     Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
     notify_by_smtp options.reverse_merge({
         :address        => 'smtp.gmail.com',
@@ -27,11 +29,14 @@ module God
   #
   # SMTP email
   #
+  # Assumes that the 'from' account and the smtp server login name are identical.
   def self.notify_by_smtp options
+    God::Contacts::Email.message_settings = {
+      :from           => options[:username], }
     God::Contacts::Email.server_settings = {
-      :address        => options[:smtp_address],
-      :port           => 25,
-      :user_name      => options[:from],
+      :address        => options[:address],
+      :port           => options[:port] || 25,
+      :user_name      => options[:username],
       :password       => options[:password],
       :domain         => options[:domain],
       :authentication => :plain
